a0c94780526bdfe241bc7fc65ad2f3ecf10a4c47
Angelo Mottola <a.mottola@converge.it>
2019-06-10 17:08:56 +0200

    Small update

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ if sys.platform == 'darwin':
 elif sys.platform == 'win32':
 	suffix = '_d' if debug else ''
 	cflags = '/EHsc /D_CRT_SECURE_NO_WARNINGS /Z7 /wd4244 /wd4005 /wd4267'
-	ldflags = '/DEBUG /NODEFAULTLIB:LIBCMT /ignore:4197'
+	ldflags = '/DEBUG /NODEFAULTLIB:LIBCMT /ignore:4197 /ignore:4099'
 	extra_libs = 'ebpr_s%s konga_client_s%s zlib%s shell32 user32 netapi32 iphlpapi shlwapi advapi32 secur32 ws2_32' % (suffix, suffix, suffix[1:])
 	if konga_sdk is not None:
 		cflags += ' /I"%s\\Include"' % konga_sdk
