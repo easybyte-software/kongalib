c6c7db574952987a9486ae3925ca5566713c1a5f
Angelo Mottola <a.mottola@converge.it>
2019-06-19 15:23:12 +0200

    azure wheel support WIP

---
+++ azure-wheel-helpers/build-deps.bat
@@ -0,0 +1,46 @@
+@echo off
+
+if "%2" neq "" (
+    call "%2" "%3"
+)
+
+echo Preparing third_party
+set "ROOT=%~dp0\.."
+cd %ROOT%
+md third_party
+cd third_party
+md include
+md bin
+md lib
+
+
+echo Compiling libmpdec
+cd "%ROOT%\mpdecimal-2.4.2\vcbuild"
+call "vcbuild%1"
+copy /Y "dist%1\libmpdec-2.4.2.lib" "%ROOT%\third_party\lib\mpdec.lib"
+copy /Y dist%1\*.h "%ROOT%\third_party\include"
+copy /Y ..\libmpdec\vc*.h "%ROOT%\third_party\include"
+dir /s "%ROOT%\third_party"
+
+echo Compiling zlib
+cd "%ROOT%\zlib-1.2.11"
+nmake -f win32\Makefile.msc zlib.lib
+copy /Y zlib.lib "%ROOT%\third_party\lib"',
+copy /Y zlib.h "%ROOT%\third_party\include"',
+copy /Y zconf.h "%ROOT%\third_party\include"',
+
+echo Compiling pcre
+cd "%ROOT%\pcre-8.43"
+md out
+cd out
+cmake -G "NMake Makefiles" -DPCRE_SUPPORT_UTF=On -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%ROOT%\third_party ..
+nmake
+nmake install
+
+echo Compiling ebpr and konga_client
+cd "%ROOT%\konga"
+md out
+cd out
+cmake .. -DOPT_USE_CPP11=1 -DOPT_NO_SSL=1 -DOPT_KONGALIB_WHEEL=1 -DTHIRD_PARTY="%ROOT%\third_party" -G "NMake Makefiles"
+nmake
+nmake install
