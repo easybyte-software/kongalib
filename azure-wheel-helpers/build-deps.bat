a9f227846ed686dbc87a35c6ab8a46f52b5d0b9d
Angelo Mottola <a.mottola@converge.it>
2019-06-19 16:53:53 +0200

    azure wheel support WIP

--- azure-wheel-helpers/build-deps.bat
+++ azure-wheel-helpers/build-deps.bat
@@ -1,48 +1,46 @@
 @setlocal enableextensions enabledelayedexpansion
 @echo off
 
-if "%2" neq "" (
-	echo Calling vcvarsall
-    call "C:\Program Files (x86)\Common Files\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat" %2
+if "%PYTHON_VERSION%"=="2.7" (
+	echo Calling Python 2.7 vcvarsall
+    call "C:\Program Files (x86)\Common Files\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat" "%PYTHON_ARCHITECTURE%"
 )
 
 echo Preparing third_party
-set "ROOT=%~dp0\.."
-cd %ROOT%
+cd %BUILD_SOURCESDIRECTORY%
 md third_party
 cd third_party
 md include
 md bin
 md lib
 
-
 echo Compiling libmpdec
-cd "%ROOT%\mpdecimal-2.4.2\vcbuild"
-call "vcbuild%1"
-copy /Y "dist%1\libmpdec-2.4.2.lib" "%ROOT%\third_party\lib\mpdec.lib"
-copy /Y dist%1\*.h "%ROOT%\third_party\include"
-copy /Y ..\libmpdec\vc*.h "%ROOT%\third_party\include"
-dir /s "%ROOT%\third_party"
+cd "%BUILD_SOURCESDIRECTORY%\mpdecimal-2.4.2\vcbuild"
+call "vcbuild%MPDEC_BITS%"
+copy /Y "dist%MPDEC_BITS%\libmpdec-2.4.2.lib" "%BUILD_SOURCESDIRECTORY%\third_party\lib\mpdec.lib"
+copy /Y dist%MPDEC_BITS%\*.h "%BUILD_SOURCESDIRECTORY%\third_party\include"
+copy /Y ..\libmpdec\vc*.h "%BUILD_SOURCESDIRECTORY%\third_party\include"
+dir /s "%BUILD_SOURCESDIRECTORY%\third_party"
 
 echo Compiling zlib
-cd "%ROOT%\zlib-1.2.11"
+cd "%BUILD_SOURCESDIRECTORY%\zlib-1.2.11"
 nmake -f win32\Makefile.msc zlib.lib
-copy /Y zlib.lib "%ROOT%\third_party\lib"
-copy /Y zlib.h "%ROOT%\third_party\include"
-copy /Y zconf.h "%ROOT%\third_party\include"
+copy /Y zlib.lib "%BUILD_SOURCESDIRECTORY%\third_party\lib"
+copy /Y zlib.h "%BUILD_SOURCESDIRECTORY%\third_party\include"
+copy /Y zconf.h "%BUILD_SOURCESDIRECTORY%\third_party\include"
 
 echo Compiling pcre
-cd "%ROOT%\pcre-8.31"
+cd "%BUILD_SOURCESDIRECTORY%\pcre-8.31"
 md out
 cd out
-cmake -G "NMake Makefiles" -DPCRE_SUPPORT_UTF=On -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%ROOT%\third_party ..
+cmake -G "NMake Makefiles" -DPCRE_SUPPORT_UTF=On -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%BUILD_SOURCESDIRECTORY%\third_party ..
 nmake
 nmake install
 
 echo Compiling ebpr and konga_client
-cd "%ROOT%\konga"
+cd "%BUILD_SOURCESDIRECTORY%\konga"
 md out
 cd out
-cmake .. -DOPT_USE_CPP11=1 -DOPT_NO_SSL=1 -DOPT_KONGALIB_WHEEL=1 -DTHIRD_PARTY="%ROOT%\third_party" -G "NMake Makefiles"
+cmake .. -DOPT_USE_CPP11=1 -DOPT_NO_SSL=1 -DOPT_KONGALIB_WHEEL=1 -DTHIRD_PARTY="%BUILD_SOURCESDIRECTORY%\third_party" -G "NMake Makefiles"
 nmake
 nmake install
