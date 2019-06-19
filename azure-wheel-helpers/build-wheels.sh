2f92a170650f4fb096c8dbdb8a93b99fff3ed11b
Angelo Mottola <a.mottola@converge.it>
2019-06-19 11:01:48 +0200

    azure wheel support WIP

--- azure-wheel-helpers/build-wheels.sh
+++ azure-wheel-helpers/build-wheels.sh
@@ -7,14 +7,13 @@ yum install -y zlib-devel pcre-devel dbus-devel dbus-libs cmake
 cd /io/mpdecimal-2.4.2
 ./configure
 CFLAGS=-fPIC make -j2
-sudo make install
+make install
 
 cd /io/konga
 mkdir -p out
 cd out
 cmake .. -DOPT_USE_CPP11=1 -DOPT_NO_SSL=1 -DOPT_KONGALIB_WHEEL=1
-make -j2
-sudo make install
+make -j2 kongalib_wheel
 
 cd /io
 rm -fr build dist *.egg-info
