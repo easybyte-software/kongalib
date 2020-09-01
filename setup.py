2cf9999b9cff952719817f47b33d988b5d15f838
Angelo Mottola <a.mottola@converge.it>
2020-09-01 18:01:42 +0200

    Possible build fix

--- setup.py
+++ setup.py
@@ -109,8 +109,12 @@ elif sys.platform == 'win32':
 	ldflags = '/DEBUG /NODEFAULTLIB:LIBCMT /ignore:4197 /ignore:4099'
 	extra_libs = 'ebpr_s%s konga_client_s%s zlib%s shell32 user32 netapi32 iphlpapi shlwapi advapi32 secur32 ws2_32' % (suffix, suffix, suffix[1:])
 	if konga_sdk is not None:
-		cflags += ' /I"%s\\Include"' % konga_sdk
-		ldflags += ' /LIBPATH:"%s\\Lib"' % konga_sdk
+		if ' ' in konga_sdk:
+			cflags += ' /I"%s\\Include"' % konga_sdk
+			ldflags += ' /LIBPATH:"%s\\Lib"' % konga_sdk
+		else:
+			cflags += ' /I%s\\Include' % konga_sdk
+			ldflags += ' /LIBPATH:%s\\Lib' % konga_sdk
 else:
 	if konga_sdk is None:
 		konga_sdk = '/usr/local'
