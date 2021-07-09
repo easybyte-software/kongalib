8e74e2db41038b0cfbcaac3701e404a05db2e51d
Angelo Mottola <a.mottola@converge.it>
2021-07-09 14:32:36 +0200

    WIP

--- .azure-pipelines/macos-install-python.sh
+++ .azure-pipelines/macos-install-python.sh
@@ -4,27 +4,22 @@ case $PYTHON_VERSION in
 2.7)
   FULL_VERSION=2.7.16
   INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
-  EXE=python
   ;;
 3.6)
   FULL_VERSION=3.6.8
   INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
-  EXE=python3
   ;;
 3.7)
   FULL_VERSION=3.7.6
   INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
-  EXE=python3
   ;;
 3.8)
   FULL_VERSION=3.8.7
   INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
-  EXE=python3
   ;;
 3.9)
   FULL_VERSION=3.9.6
   INSTALLER_NAME=python-$FULL_VERSION-macos11.pkg
-  EXE=python3
   ;;
 esac
 
@@ -39,7 +34,7 @@ curl $URL > $INSTALLER_NAME
 sudo installer -pkg $INSTALLER_NAME -target /
 
 sudo rm /usr/local/bin/python
-sudo ln -s $PY_PREFIX/$PYTHON_VERSION/bin/$EXE /usr/local/bin/python
+sudo ln -s $PY_PREFIX/$PYTHON_VERSION/bin/python$PYTHON_VERSION /usr/local/bin/python
 
 which python
 python --version
