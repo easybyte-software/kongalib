08378fe431816f17ab68cd2a20caceb24abefb5a
Angelo Mottola <a.mottola@converge.it>
2021-07-09 09:51:33 +0200

    Try to build mac universal2 wheels

--- .azure-pipelines/macos-install-python.sh
+++ .azure-pipelines/macos-install-python.sh
@@ -3,22 +3,26 @@ PYTHON_VERSION="$1"
 case $PYTHON_VERSION in
 2.7)
   FULL_VERSION=2.7.16
+  INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
   ;;
 3.6)
   FULL_VERSION=3.6.8
+  INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
   ;;
 3.7)
   FULL_VERSION=3.7.6
+  INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
   ;;
 3.8)
   FULL_VERSION=3.8.7
+  INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
   ;;
 3.9)
-  FULL_VERSION=3.9.1
+  FULL_VERSION=3.9.6
+  INSTALLER_NAME=python-$FULL_VERSION-macosx11.pkg
   ;;
 esac
 
-INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
 URL=https://www.python.org/ftp/python/$FULL_VERSION/$INSTALLER_NAME
 
 PY_PREFIX=/Library/Frameworks/Python.framework/Versions
