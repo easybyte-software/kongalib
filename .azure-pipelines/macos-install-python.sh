5a2e9788b6a95c598aeab31249a1615bd74692a1
Angelo Mottola <a.mottola@converge.it>
2021-07-09 12:07:37 +0200

    Ensure we use correct python version

--- .azure-pipelines/macos-install-python.sh
+++ .azure-pipelines/macos-install-python.sh
@@ -34,7 +34,7 @@ curl $URL > $INSTALLER_NAME
 sudo installer -pkg $INSTALLER_NAME -target /
 
 sudo rm /usr/local/bin/python
-sudo ln -s /usr/local/bin/python$PYTHON_VERSION /usr/local/bin/python
+sudo ln -s /Library/Frameworks/Python.framework/Versions/$PYTHON_VERSION/bin/python3 /usr/local/bin/python
 
 which python
 python --version
