33a66bc6e7da9b8182f2dbd5856ba3d63b433a19
Angelo Mottola <a.mottola@converge.it>
2023-01-30 12:15:46 +0100

    CI fixes

--- .azure-pipelines/macos-install-python.sh
+++ .azure-pipelines/macos-install-python.sh
@@ -1,25 +1,25 @@
 PYTHON_VERSION="$1"
 
 case $PYTHON_VERSION in
-2.7)
-  FULL_VERSION=2.7.16
-  INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
-  ;;
 3.7)
-  FULL_VERSION=3.7.6
+  FULL_VERSION=3.7.16
   INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
   ;;
 3.8)
-  FULL_VERSION=3.8.7
+  FULL_VERSION=3.8.9
   INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
   ;;
 3.9)
-  FULL_VERSION=3.9.7
+  FULL_VERSION=3.9.9
   INSTALLER_NAME=python-$FULL_VERSION-macos11.pkg
   ;;
 3.10)
-  FULL_VERSION=3.10.0
-  INSTALLER_NAME=python-${FULL_VERSION}post2-macos11.pkg
+  FULL_VERSION=3.10.9
+  INSTALLER_NAME=python-${FULL_VERSION}-macos11.pkg
+  ;;
+3.11)
+  FULL_VERSION=3.11.1
+  INSTALLER_NAME=python-${FULL_VERSION}-macos11.pkg
   ;;
 esac
 
@@ -38,8 +38,4 @@ sudo ln -s $PY_PREFIX/$PYTHON_VERSION/bin/python$PYTHON_VERSION /usr/local/bin/p
 
 which python
 python --version
-if [ "$PYTHON_VERSION" == "2.7" ]; then
-	python -m ensurepip
-fi
-
 python -m pip install setuptools twine wheel
