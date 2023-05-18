b41de2a3e3392f7480931cf03df95daba9270470
Angelo Mottola <a.mottola@converge.it>
2023-05-18 15:12:48 +0200

    Small fix

--- setup.py
+++ setup.py
@@ -84,8 +84,8 @@ elif sys.platform == 'win32':
 	extra_libs = 'ebpr_s konga_client_s shell32 user32 netapi32 iphlpapi shlwapi advapi32 secur32 ws2_32 psapi bcrypt'
 	if konga_sdk is not None:
 		if ' ' in konga_sdk:
-			cflags += ' /I"%s\\Include"' % konga_sdk
-			ldflags += ' /LIBPATH:"%s\\Lib"' % konga_sdk
+			cflags += ' "-I%s\\Include"' % konga_sdk
+			ldflags += ' "-LIBPATH:%s\\Lib"' % konga_sdk
 		else:
 			cflags += ' /I%s\\Include' % konga_sdk
 			ldflags += ' /LIBPATH:%s\\Lib' % konga_sdk
