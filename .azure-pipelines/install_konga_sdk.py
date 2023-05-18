0f6e0ba3588636cb119fad3a05b5ebfd7f290959
Angelo Mottola <a.mottola@converge.it>
2023-05-18 11:36:27 +0200

    Fixes

--- .azure-pipelines/install_konga_sdk.py
+++ .azure-pipelines/install_konga_sdk.py
@@ -1,6 +1,16 @@
 import sys
-import urllib.request
+import re
 import subprocess
+import requests
+
+
+def get_filename_from_cd(cd):
+	if not cd:
+		return None
+	fname = re.findall('filename=(.+)', cd)
+	if len(fname) == 0:
+		return None
+	return fname[0]
 
 
 def main(tag, operating_system):
@@ -12,14 +22,18 @@ def main(tag, operating_system):
 		else:
 			tag = 'archive/%s' % tag
 	url = 'https://public.easybyte.it/downloads/%s/sdk?os=%s' % (tag, operating_system)
-	path = urllib.request.urlretrieve(url)[0]
+
+	r = requests.get(url, allow_redirects=True)
+	path = get_filename_from_cd(r.headers.get('content-disposition'))
+	with open(path, 'wb') as f:
+		f.write(r.content)
 	print('Downloaded SDK into %s' % path)
 	if operating_system == 'mac':
 		cmd = 'sudo installer -pkg %s -target /' % path
 	elif operating_system == 'win':
 		cmd = '%s /install /quiet' % path
 	else:
-		cmd = 'sudo dpkg -i %s' % path
+		cmd = 'sudo apt install %s && sudo apt install -f' % path
 	subprocess.run(cmd, shell=True)
 
 
