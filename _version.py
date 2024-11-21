40646f8e86c08040ff294d187cc69e82684c08cf
Angelo Mottola <a.mottola@converge.it>
2024-11-21 11:29:15 +0100

    Better to use 2.0.0-1 as version

--- _version.py
+++ _version.py
@@ -1,14 +1,14 @@
 import os
 from setuptools import build_meta as build_meta
 
-_VERSION = '2.0.1'
+_VERSION = '2.0.0-1'
 
 
 def __getattr__(name):
 	if name == '__version__':
 		version_build = os.environ.get('KONGALIB_BUILD', None)
 		if version_build:
-			return '%s-%s' % (_VERSION, version_build)
+			return '%s-%s' % (_VERSION.split('-')[0], version_build)
 		else:
 			return _VERSION
 		
