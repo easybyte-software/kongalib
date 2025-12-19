c63234289b820f19b528f699a7e7bbbf9b25e61a
Angelo Mottola <a.mottola@converge.it>
2025-12-19 12:25:12 +0100

    Added support for uploading wheels to KSS

---
+++ .github/scripts/publish_to_kss.py
@@ -0,0 +1,65 @@
+import sys
+import os
+import argparse
+import binascii
+import re
+import io
+
+import requests
+import rsa
+
+
+SUPPORT_SERVER_PUBLISH_UPLOAD	= 'https://kss-secure.easybyte.it/service/publish_upload'
+
+
+def main():
+	parser = argparse.ArgumentParser(description='Publish installers to KSS.')
+	parser.add_argument('--private-key', type=str, required=True, help='Private key for signing uploads.')
+	parser.add_argument('--sdk-version', type=str, required=True, help='Version tag for the upload.')
+	parser.add_argument('--public', action='store_true', help='Make the upload public.')
+	parser.add_argument('installers', nargs='+', help='Paths to installer files to upload.')
+	options = parser.parse_args()
+
+	private_key = rsa.PrivateKey.load_pkcs1(binascii.a2b_base64(options.private_key))
+
+	for archive in options.installers:
+		spec = archive.split('-')[-1]
+		if re.match(r'^macos_\d+_\d+_universal2.whl$', archive):
+			platform = 'u'
+		elif re.match(r'^manylinux_\d+_\d+_aarch64.whl$', archive):
+			platform = 'a'
+		elif re.match(r'^manylinux_\d+_\d+_x86_64.whl$', archive):
+			platform = 'l'
+		elif re.match(r'^win_amd64.whl$', archive):
+			platform = 'w'
+		else:
+			platform = ''
+
+		with open(archive, 'rb') as file:
+			content = file.read()
+		signature = rsa.sign(content, private_key, 'SHA-1')
+
+		if options.sdk_version == 'nightly':
+			tag = version = ''
+			channel = 'nightly'
+		else:
+			channel, version = options.sdk_version.split('/')
+			tag = version
+
+		response = requests.post(SUPPORT_SERVER_PUBLISH_UPLOAD, data={
+			'signature': binascii.hexlify(signature),
+			'public': 'true' if (channel == 'archive') else 'false',
+			'dist': 'true' if (channel == 'archive') and options.public else 'false',
+			'tag': tag,
+			'version': version,
+			'activation_version': 0,
+			'target': 'kongalib',
+			'platform': platform,
+		}, files={
+			'installer_file': ( os.path.basename(archive), io.BytesIO(content) ),
+		})
+		response.raise_for_status()
+
+
+if __name__ == '__main__':
+	main()
